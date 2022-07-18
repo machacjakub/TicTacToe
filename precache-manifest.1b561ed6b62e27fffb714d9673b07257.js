b74656270748109eb1d1ac10123037bc06be25fe
machacjakub <j.machac@post.cz>
2022-07-18 12:05:02 +0200

    Updates

---
+++ precache-manifest.1b561ed6b62e27fffb714d9673b07257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c6bbe5c5ce1b0c99b73",
+    "url": "/TicTacToe/static/css/main.075fc50d.chunk.css"
+  },
+  {
+    "revision": "3c6bbe5c5ce1b0c99b73",
+    "url": "/TicTacToe/static/js/main.3c6bbe5c.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/TicTacToe/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "cf35085d877dfd49fcb3",
+    "url": "/TicTacToe/static/js/2.cf35085d.chunk.js"
+  },
+  {
+    "revision": "773dc9167b1019f65ee4",
+    "url": "/TicTacToe/static/js/runtime~main.773dc916.js"
+  },
+  {
+    "revision": "caa601b8ce00c4a249b18c66ad7ab3a1",
+    "url": "/TicTacToe/static/media/logo.caa601b8.png"
+  },
+  {
+    "revision": "c796876511174e3672a6ddca67edbbf3",
+    "url": "/TicTacToe/index.html"
+  }
+];
